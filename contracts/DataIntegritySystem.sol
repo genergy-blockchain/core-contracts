3c1247408b4c6b50780877135a09666d8237f6aa
rod <vvolfkind@gmail.com>
2023-10-06 15:35:40 -0300

    initial scaffolding

--- contracts/DataIntegritySystem.sol
+++ contracts/DataIntegritySystem.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CensusSystem {
+contract DataIntegritySystem {
     struct Checksum {
         uint256 id;
         string verificationHash;
